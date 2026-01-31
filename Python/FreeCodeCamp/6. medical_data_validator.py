9a79a7498a096acf403e68f2403fc7d2abacd381
unknown <qarlosbaldovino@gmail.com>
2026-01-31 11:25:13 -0300

    terminado ejericio 6.

--- Python/FreeCodeCamp/6. medical_data_validator.py
+++ Python/FreeCodeCamp/6. medical_data_validator.py
@@ -39,45 +39,53 @@ def find_invalid_records(
     patient_id, age, gender, diagnosis, medications, last_visit_id
 ):
     constraints = {
-        'patient_id': isinstance(patient_id, str)
-        and re.fullmatch('p\d+', patient_id, re.IGNORECASE),
-        'age': isinstance(age, int) and age >= 18,
-        'gender': isinstance(gender, str) and gender.lower() in ('male', 'female'),
-        'diagnosis': isinstance(diagnosis, str) or diagnosis is None,
+        'patient_id': isinstance(patient_id, str) 
+        and re.fullmatch(r'p\d+', patient_id, re.IGNORECASE), #String y empezar con una (p o P) y despues numeros.
+        'age': isinstance(age, int) and age >= 18, #Entero y mayor igual a 18
+        'gender': isinstance(gender, str) and gender.lower() in ('male', 'female'), #String y el genero (pasado a minuscula) male o female
+        'diagnosis': isinstance(diagnosis, str) or diagnosis is None, #String o None
         'medications': isinstance(medications, list)
-        and all([isinstance(i, str) for i in medications]),
+        and all([isinstance(i, str) for i in medications]), #Array y todos los valores dentro deben ser string
         'last_visit_id': isinstance(last_visit_id, str)
-        and re.fullmatch('v\d+', last_visit_id, re.IGNORECASE)
+        and re.fullmatch(r'v\d+', last_visit_id, re.IGNORECASE) #String y empezar con V y numeros
     }
-    return [key for key, value in constraints.items() if not value]
+
+    return [key for key, value in constraints.items() if not value] #Devuelve la clave de los items que no cumplen
 
 def validate(data):
-    is_sequence = isinstance(data, (list, tuple))
+    is_sequence = isinstance(data, (list, tuple)) #Verifica si pasamos una lista o tupla
+    is_invalid = False
 
-    if not is_sequence:
+    if not is_sequence: #Si no es lista o tupla...
         print('Invalid format: expected a list or tuple.')
         return False
-        
-    is_invalid = False
-    key_set = set(
+
+    key_set = set( #Seteamos con las claves de lo que se espera como diccionario
         ['patient_id', 'age', 'gender', 'diagnosis', 'medications', 'last_visit_id']
     )
 
-    for index, dictionary in enumerate(data):
+    for index, dictionary in enumerate(data): #Iteramos la data extrayendo el índice y su contenido
+
         if not isinstance(dictionary, dict):
             print(f'Invalid format: expected a dictionary at position {index}.')
             is_invalid = True
 
-        if set(dictionary.keys()) != key_set:
+        if set(dictionary.keys()) != key_set: #Chequeamos si las claves son iguales al set que hicimos con la data que pasamos como parametro
             print(
                 f'Invalid format: {dictionary} at position {index} has missing and/or invalid keys.'
             )
             is_invalid = True
 
-        print(*data[index].values())
-    
+        invalid_records = find_invalid_records(**dictionary) #Guardamos en una lista las keys con errores
+
+        for key in invalid_records: #Si encuentra error, mostramos la lista y el valor donde se equivoca y su respectiva posicion
+            print(f"Unexpected format '{key}: {dictionary[key]}' at position {index}.")
+            is_invalid = True
+        
+
     if is_invalid:
         return False
+    
     print('Valid format.')
     return True
 
